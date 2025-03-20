fdda6c39cbf550f36bcddf0e8108cf5c28ac8f30
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-20 20:34:59 +0100

    Adding Wraithstalker base

---
+++ CustomHeroes/Wraithstalker.cs
@@ -0,0 +1,75 @@
+﻿using CounterStrikeSharp.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using WarcraftPlugin.Models;
+
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API;
+using Vector = CounterStrikeSharp.API.Modules.Utils.Vector;
+using CounterStrikeSharp.API.Modules.Memory;
+using WarcraftPlugin.Helpers;
+using WarcraftPlugin.Models;
+using System.Drawing;
+using WarcraftPlugin.Core.Effects;
+using System.Collections.Generic;
+using WarcraftPlugin.Events.ExtendedEvents;
+using System;
+using System.Reflection;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using WarcraftPlugin.Core;
+using WarcraftPlugin.Summons;
+
+
+namespace WarcraftPlugin.Classes
+{
+    public class Wraithstalker : WarcraftClass
+    {
+        public override string DisplayName => "Wraithstalker";
+
+        private readonly WarcraftPlugin _plugin;
+        public override Color DefaultColor => Color.CadetBlue;
+
+        public override List<IWarcraftAbility> Abilities =>
+        [
+            new WarcraftAbility("Assimilation", "On Kill: Movement speed and reduced gravity for 3/5/7/9/10 seconds, this also grants a blindstack max 2, which blinds an enemy when he spots you."),
+            new WarcraftAbility("Phantom Cloak", "Standing still for  2.5 - 0.5 seconds makes you invisible and your next shot deals bonus damage."),
+            new WarcraftAbility("Shadowstrike", "After you exited Phantom Cloak your next hit will cause bonus damage and grant you a guaranteed skull"),
+            new WarcraftCooldownAbility("Marked for prey", "Scan the area where you are looking, highlight enemies close for x seconds and slow them down. Killing a marked target grants a skull. Skulls give you lasting benefits untill mapchange.", 60f)
+        ];
+
+        public override void Register()
+        {
+            HookEvent<EventPlayerSpawn>(PlayerSpawn);
+
+            HookAbility(3, Ultimate);
+        }
+        private void PlayerSpawn(EventPlayerSpawn spawn)
+        {
+            
+        }
+
+        private void Ultimate()
+        {
+           StartCooldown(3);
+        }
+
+        private void PlayerShoot(EventWeaponFire @event)
+        {
+           
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
