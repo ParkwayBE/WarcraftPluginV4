ca74b5ea959257d965fcc888ffad8012ecc52946
ParkwayBE <vandammeben@outlook.com>
2025-03-20 21:51:36 +0100

    dom

--- Summons/FootBall.cs
+++ Summons/FootBall.cs
@@ -49,7 +49,7 @@ namespace WarcraftPlugin.Summons
             var distance = 60;
             var height = 10;
             posInfrontOfPlayer = owner.CalculatePositionInFront(distance, height);
-            _ball.Teleport(posInfrontOfPlayer, owner.PlayerPawn.Value.V_angle, new Vector(nint.Zero));
+            _ballProp.Teleport(posInfrontOfPlayer, owner.PlayerPawn.Value.V_angle, new Vector(nint.Zero));
            
         }
     }
