449ed7b0056067f7ac3d48fe887f54f2a241557a
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-20 18:38:05 +0100

    Trying dog bark sound

--- CustomHeroes/Naix.cs
+++ CustomHeroes/Naix.cs
@@ -323,8 +323,8 @@ namespace WarcraftPlugin.Classes
             }
             killer.PlayerPawn.Value.SetColor(Color.Red);
             SpawnParticles();
-            killer.PlayLocalSound("sounds/ambient/ambience/rainscapes/thunder_close04.vsnd");
-            victim.PlayLocalSound("sounds/ambient/ambience/rainscapes/thunder_close04.vsnd");
+            killer.PlayLocalSound("sounds/ambient/ambience/creatures/dog_bark_close.vsnd");
+            victim.PlayLocalSound("sounds/ambient/ambience/creatures/dog_bark_close.vsnd");
 
 
             WarcraftPlugin.Instance.AddTimer(3.0f, () =>
