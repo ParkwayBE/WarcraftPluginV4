ce3c21fd4535ffa3a8e6148ef2a42fc79e09250b
ParkwayBE <vandammeben@outlook.com>
2025-03-20 18:01:06 +0100

    footballtest

---
+++ CustomHeroes/Lukaku.cs
@@ -0,0 +1,85 @@
+﻿using CounterStrikeSharp.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using WarcraftPlugin.Models;
+
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API;
+using Vector = CounterStrikeSharp.API.Modules.Utils.Vector;
+using CounterStrikeSharp.API.Modules.Memory;
+using WarcraftPlugin.Helpers;
+using WarcraftPlugin.Models;
+using System.Drawing;
+using WarcraftPlugin.Core.Effects;
+using System.Collections.Generic;
+using WarcraftPlugin.Events.ExtendedEvents;
+using System;
+using System.Reflection;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using WarcraftPlugin.Core;
+using WarcraftPlugin.Summons;
+
+
+namespace WarcraftPlugin.Classes
+{
+    public class Lukaku : WarcraftClass
+    {
+        public override string DisplayName => "Lukaku";
+
+        private readonly WarcraftPlugin _plugin;
+        public override Color DefaultColor => Color.CadetBlue;
+
+        List<FootBall> footBalls = new List<FootBall>();
+
+
+        public override List<IWarcraftAbility> Abilities =>
+        [
+            new WarcraftAbility("DoSomething", "Quick"),
+            new WarcraftAbility("Monkey Agility", "Increases movement speed and evasion."),
+            new WarcraftAbility("Primal Roar", "Emits a roar when killing an enemy that stuns nearby enemies."),
+            new WarcraftCooldownAbility("Ballr", "Balls", 60f)
+        ];
+
+        public override void Register()
+        {
+
+            HookEvent<EventWeaponFire>(PlayerShoot);
+            HookAbility(3, Ultimate);
+        }
+        private void PlayerSpawn(EventPlayerSpawn spawn)
+        {
+            footBalls.Add(new FootBall(Player, Player.PlayerPawn.Value.AbsOrigin));
+            footBalls.Add(new FootBall(Player, Player.PlayerPawn.Value.AbsOrigin));
+            footBalls.Add(new FootBall(Player, Player.PlayerPawn.Value.AbsOrigin));
+            Player.PrintToChat("you have 3 footballs");
+        }
+
+        private void Ultimate()
+        {
+            Console.WriteLine("Lukaku used ultimate!");
+            footBalls[0].Activate();
+            footBalls.RemoveAt(footBalls.Count - 1);
+            
+
+        }
+
+        private void PlayerShoot(EventWeaponFire @event)
+        {
+            // _plugin.AdminPanel.OpenAdminPanel(Player);
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
