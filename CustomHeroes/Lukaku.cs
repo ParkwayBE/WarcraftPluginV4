ca86cb5297a9e1642d97b02a045c368ea77c3c00
ParkwayBE <vandammeben@outlook.com>
2025-03-20 21:11:33 +0100

    gogoo

--- CustomHeroes/Lukaku.cs
+++ CustomHeroes/Lukaku.cs
@@ -40,7 +40,7 @@ namespace WarcraftPlugin.Classes
             new WarcraftAbility("DoSomething", "Quick"),
             new WarcraftAbility("Monkey Agility", "Increases movement speed and evasion."),
             new WarcraftAbility("Primal Roar", "Emits a roar when killing an enemy that stuns nearby enemies."),
-            new WarcraftCooldownAbility("Ballr", "Balls", 60f)
+            new WarcraftCooldownAbility("Ballr", "Balls", 0.5f)
         ];
 
         public override void Register()
