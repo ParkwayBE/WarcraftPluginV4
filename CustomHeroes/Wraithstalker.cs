52f5161e0d313f42810ff1fe4e28d70dfa0427a4
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-21 23:43:22 +0100

    updated weapon pickup prevention logic.

--- CustomHeroes/Wraithstalker.cs
+++ CustomHeroes/Wraithstalker.cs
@@ -61,6 +61,7 @@ namespace WarcraftPlugin.Classes
             HookEvent<EventPlayerSpawn>(PlayerSpawn);
             HookEvent<EventPlayerDeath>(OnPlayerDeath);
             HookEvent<EventRoundEnd>(OnRoundEnd);
+            HookEvent<EventRoundStart>(OnRoundStart);
             HookEvent<EventPlayerHurtOther>(PlayerHurtOther);
             HookEvent<EventPlayerDisconnect>(PlayerDisconnect);
             HookEvent<EventPlayerConnect>(OnPlayerConnect);
@@ -69,7 +70,14 @@ namespace WarcraftPlugin.Classes
             HookAbility(3, Ultimate);
         }
 
-
+        private void OnRoundStart(EventRoundStart @event)
+        {
+            WarcraftPlugin.Instance.AddTimer(0.5f, () =>
+            {
+                ApplySkullBonuses(Player);
+                Player.PlayerPawn.Value.WeaponServices.PreventWeaponPickup = true;
+            });
+        }
 
         private void PlayerHurtOther(EventPlayerHurtOther @event)
         {
@@ -254,16 +262,6 @@ namespace WarcraftPlugin.Classes
             {
                 Player.GiveNamedItem("weapon_ssg08");
             }
-
-
-            WarcraftPlugin.Instance.AddTimer(0.5f, () =>
-            {
-                ApplySkullBonuses(Player);
-                Player.PlayerPawn.Value.WeaponServices.PreventWeaponPickup = true;
-            });
-            
-
-
         }
 
 
