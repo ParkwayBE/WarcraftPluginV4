d71c62c21c5b9f3eacdf23ef5e0b3ce5193c54b3
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-22 00:12:14 +0100

    testing spawn effect.

--- CustomHeroes/Wraithstalker.cs
+++ CustomHeroes/Wraithstalker.cs
@@ -266,6 +266,54 @@ namespace WarcraftPlugin.Classes
             {
                 Player.GiveNamedItem("weapon_ssg08");
             }
+
+            SpawnParticles();
+        }
+
+        int repetitionCount = 0;
+        int maxRepetitions = 5;
+        float delayBetweenRepetitions = 2.0f;
+
+        void SpawnParticles()
+        {
+            // EFFECT CODE
+            float offset = 150.0f; // Adjust the offset as needed
+            float particleDuration = 120.0f;
+            float particleDuration2 = 40.0f;
+            string redCircleParticle = "particles/lighting/light_gaslamp_glow.vpcf";
+            string redCircleParticle2 = "particles/inferno_fx/explosion_incend_air_core.vpcf";
+
+            var basePosition = Player.PlayerPawn.Value.AbsOrigin.Clone();
+            basePosition.Z += 50; // Raise all particles above the ground
+
+            // Spawn particle 1 (center)
+            var particle1 = Warcraft.SpawnParticle(basePosition, redCircleParticle, particleDuration);
+            particle1.SetParent(Player.PlayerPawn.Value);
+
+            // Spawn particle 2 (offset slightly in X)
+            var particle2Position = basePosition.Clone();
+            particle2Position.X += offset;
+            var particle2 = Warcraft.SpawnParticle(particle2Position, redCircleParticle, particleDuration);
+            particle2.SetParent(Player.PlayerPawn.Value);
+
+            // Spawn particle 3 (offset slightly in Y)
+            var particle3Position = basePosition.Clone();
+            particle3Position.Y += offset;
+            var particle3 = Warcraft.SpawnParticle(particle3Position, redCircleParticle, particleDuration);
+            particle3.SetParent(Player.PlayerPawn.Value);
+
+            // particle 4 
+            var particle4Position = basePosition.Clone();
+            var particle4 = Warcraft.SpawnParticle(particle4Position, redCircleParticle2, particleDuration2);
+            particle4.SetParent(Player.PlayerPawn.Value);
+
+            // END EFFECT CODE
+
+            repetitionCount++;
+            if (repetitionCount < maxRepetitions)
+            {
+                WarcraftPlugin.Instance.AddTimer(delayBetweenRepetitions, SpawnParticles);
+            }
         }
 
 
