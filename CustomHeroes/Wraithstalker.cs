4a8da15fbdc71c3d58deca1778fd2b6cd2a08f74
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-21 15:22:49 +0100

    updated , error fixes

--- CustomHeroes/Wraithstalker.cs
+++ CustomHeroes/Wraithstalker.cs
@@ -254,12 +254,12 @@ namespace WarcraftPlugin.Classes
                         _previousPosition.Y == _currentPosition.Y &&
                         _previousPosition.Z == _currentPosition.Z)
                     {
-                        CloakEnable();
+                        CloakEnable(Owner);
                         Console.WriteLine("You're standing still!");
                     }
                     else
                     {
-                        CloakDisable();
+                        CloakDisable(Owner);
                         Console.WriteLine("You are currently Moving.");
                     }
                 }, TimerFlags.REPEAT);
@@ -278,7 +278,7 @@ namespace WarcraftPlugin.Classes
         private void CloakEnable(CCSPlayerController Player)
         {
             Player.PrintToCenter("You have become 90% invisible!");
-            int abilityLevel = Owner.GetWarcraftPlayer().GetAbilityLevel(1); // Level 1–5
+            int abilityLevel = Player.GetWarcraftPlayer().GetAbilityLevel(1); // Level 1–5
             int alpha = 100 + ((5 - abilityLevel) * 20); // Level 5 = 100, Level 4 = 120, ..., Level 1 = 180
             Player.PlayerPawn.Value.SetColor(Color.FromArgb(alpha, 255, 255, 255));
 
