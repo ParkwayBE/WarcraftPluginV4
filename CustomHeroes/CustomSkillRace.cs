e615c9c6db0fcbdca0e31f0a053cb76418d65562
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-22 16:12:03 +0100

    increased slow length

--- CustomHeroes/CustomSkillRace.cs
+++ CustomHeroes/CustomSkillRace.cs
@@ -84,7 +84,7 @@ namespace WarcraftPlugin.Classes
                 return;
 
             // Example usage
-            SkillFunctions.FreezePlayer(attacker, victim, 50, 1.5f); // 25% chance to freeze for 1.5 seconds
+            SkillFunctions.FreezePlayer(attacker, victim, 50, 3.5f); // 25% chance to freeze for 1.5 seconds
         }
 
 
