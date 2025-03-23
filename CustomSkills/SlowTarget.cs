c3595ddd81f3bbdf7fb15f3dae07c87ae23858b0
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-24 00:57:15 +0100

    Removed laser from SlowEffect

--- CustomSkills/SlowTarget.cs
+++ CustomSkills/SlowTarget.cs
@@ -21,23 +21,20 @@ namespace WarcraftPlugin.CustomSkills
 
         public override void OnStart()
         {
-            _target.PrintToChat(" \x07[Freeze] You are frozen!");
+            _target.PrintToChat(" \x07[Freeze] You are slowed!");
             var pawn = _target.PlayerPawn.Value;
 
             // Half their speed
             _originalSpeed = _target.PlayerPawn.Value.VelocityModifier;
             pawn.VelocityModifier = pawn.VelocityModifier / 2;
             pawn.SetColor(Color.BlueViolet);
-
-            // Draw laser for debug/visual
-            Warcraft.DrawLaserBetween(Owner.EyePosition(-10), _target.EyePosition(-10), Color.Cyan);
         }
 
         public override void OnFinish()
         {
             _target.PlayerPawn.Value.SetColor(Color.White);
             _target.PlayerPawn.Value.VelocityModifier = _originalSpeed;
-            _target.PrintToChat(" \x07[Freeze] You are no longer frozen.");
+            _target.PrintToChat(" \x07[Freeze] You are no longer slowed.");
         }
 
         public override void OnTick() 
