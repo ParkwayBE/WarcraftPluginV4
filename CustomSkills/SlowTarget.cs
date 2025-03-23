343257393e8cb28ecb20ab830824054a0ddca273
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-24 00:49:01 +0100

    added particle effect for slow

--- CustomSkills/SlowTarget.cs
+++ CustomSkills/SlowTarget.cs
@@ -3,12 +3,15 @@ using CounterStrikeSharp.API.Core;
 using WarcraftPlugin.Core.Effects;
 using CounterStrikeSharp.API.Modules.Entities;
 using WarcraftPlugin.Helpers;
+using System;
+using WarcraftPlugin.Core.Preload;
 
 namespace WarcraftPlugin.CustomSkills
 {
     internal class SlowTarget : WarcraftEffect
     {
         private readonly CCSPlayerController _target;
+        private float _originalSpeed;
 
         public SlowTarget(CCSPlayerController owner, float duration, CCSPlayerController target)
             : base(owner, duration)
@@ -22,6 +25,7 @@ namespace WarcraftPlugin.CustomSkills
             var pawn = _target.PlayerPawn.Value;
 
             // Half their speed
+            _originalSpeed = _target.PlayerPawn.Value.VelocityModifier;
             pawn.VelocityModifier = pawn.VelocityModifier / 2;
             pawn.SetColor(Color.BlueViolet);
 
@@ -32,9 +36,14 @@ namespace WarcraftPlugin.CustomSkills
         public override void OnFinish()
         {
             _target.PlayerPawn.Value.SetColor(Color.White);
+            _target.PlayerPawn.Value.VelocityModifier = _originalSpeed;
             _target.PrintToChat(" \x07[Freeze] You are no longer frozen.");
         }
 
-        public override void OnTick() { }
+        public override void OnTick() 
+        {
+            var victimPawn = _target.PlayerPawn.Value;
+            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 50), "particles/environment/water_drip_area_01_small.vpcf", 0.4f);
+        }
     }
 }
