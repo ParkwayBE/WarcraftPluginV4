4f284f818381f294d7eb8c62a07c296c6464470e
ParkwayBE <vandammeben@outlook.com>
2025-03-24 01:51:41 +0100

    gogoo

--- AdminPanel/AdminPanel.cs
+++ AdminPanel/AdminPanel.cs
@@ -42,8 +42,9 @@ namespace WarcraftPlugin.Core
                 //player.ExecuteClientCommand($"panorama.RunScript(\"global.CSSharp_MessageEvent({{message: '{message}'}})\");");
                 string command = $"DispatchEvent('CSSharp_MessageEvent', {{ message: '{message}' }});";
                 //player.ExecuteClientCommand("GameEvents.SendCustomGameEvent('CSSharp_MessageEvent', { message: 'Hello from CSSharp!' });");
-                player.ExecuteClientCommandFromServer(command);
+                //player.ExecuteClientCommandFromServer(command);
                 Console.WriteLine("admin panel test");
+                Server.ExecuteCommand(command);
             }
             if (role == 9009)
             {
