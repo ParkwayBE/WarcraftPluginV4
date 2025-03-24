97337e9653fd96b8bcc390c20f7f96ee347b248d
Matthias <Matthias.janssens1996@hotmail.com>
2025-03-25 00:41:26 +0100

    fixed issue

--- CustomSkills/SetMovementSpeed.cs
+++ CustomSkills/SetMovementSpeed.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CounterStrikeSharp.API.Modules.Entities;
+using WarcraftPlugin.Core.Effects;
 
 // File: CustomSkills/MovementSpeed.cs
 
