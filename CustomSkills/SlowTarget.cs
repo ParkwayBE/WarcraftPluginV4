dd57b92ca6c9154099f165594c95b0c6d5a971bc
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-24 02:30:10 +0100

    Testing a lot of particles

--- CustomSkills/SlowTarget.cs
+++ CustomSkills/SlowTarget.cs
@@ -30,9 +30,46 @@ namespace WarcraftPlugin.CustomSkills
             victimPawn.VelocityModifier = victimPawn.VelocityModifier / 2;
             victimPawn.SetColor(Color.BlueViolet);
 
-            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 25), "particles/ambient_fx/snow_blizzard.vpcf", 0.4f);
-            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 50), "particles/ambient_fx/snow_blizzard.vpcf", 0.4f);
-            Warcraft.SpawnParticle(Owner.AbsOrigin.Clone().Add(z: 75), "particles/ambient_fx/snow_blizzard.vpcf", 0.4f);
+            WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+            {
+                Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/weapons/cs_weapon_fx/weapon_sensorgren_glowring.vpcf", 0.4f);
+                WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                {
+                    Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/weapons/cs_weapon_fx/weapon_molotov_fp_wick.vpcf", 0.4f); 
+                    WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                    {
+                        Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/water_impact/water_splash_01_ripple_rings_secondary.vpcf", 0.4f); 
+                        WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                        {
+                            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/water_impact/water_impact_bubbles_1.vpcf", 0.4f);
+                            WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                            {
+                                Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/water_fx/waterfall_anubis.vpcf", 0.4f); 
+                                WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                                {
+                                    Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/water_fx/water_wake_fast_ripple_sides.vpcf", 0.4f);
+                                    WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                                    {
+                                        Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/water_fx/water_wake_fast_ripple_rings_secondary.vpcf", 0.4f); 
+                                        WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                                        {
+                                            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/water_fx/water_wake_slow.vpcf", 0.4f); 
+                                            WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                                            {
+                                                Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/unified_weapon_fx/weapon_muzzleflash_basic_spark.vpcf", 0.4f);
+                                                WarcraftPlugin.Instance.AddTimer(1.5f, () =>
+                                                {
+                                                    Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/unified_weapon_fx/uweapon_muzzleflash_subm_spark.vpcf", 0.4f); 
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
         }
 
 
