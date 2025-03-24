a88fc8e6b8e956f738ce7857c41fa9f5234eaedb
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-24 01:54:55 +0100

    another snowy effect

--- CustomSkills/SlowTarget.cs
+++ CustomSkills/SlowTarget.cs
@@ -43,9 +43,9 @@ namespace WarcraftPlugin.CustomSkills
         public override void OnTick() 
         {
             var victimPawn = _target.PlayerPawn.Value;
-            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 25), "particles/weapons/cs_weapon_fx/weapon_snowball_trail.vpcf", 0.4f);
-            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 50), "particles/weapons/cs_weapon_fx/weapon_snowball_trail.vpcf", 0.4f);
-            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/weapons/cs_weapon_fx/weapon_snowball_trail.vpcf", 0.4f);
+            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 25), "particles/weapons/cs_weapon_fx/snow_hit_player_screeneffect.vpcf", 0.4f);
+            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 50), "particles/weapons/cs_weapon_fx/snow_hit_player_screeneffect.vpcf", 0.4f);
+            Warcraft.SpawnParticle(victimPawn.AbsOrigin.Clone().Add(z: 75), "particles/weapons/cs_weapon_fx/snow_hit_player_screeneffect.vpcf", 0.4f);
         }
     }
 }
