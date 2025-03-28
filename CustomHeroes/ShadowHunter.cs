610bd89601db3f9d2997059d1795fa6333534424
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-28 18:36:59 +0100

    added delay on removing the beams to allow a smoother visual

--- CustomHeroes/ShadowHunter.cs
+++ CustomHeroes/ShadowHunter.cs
@@ -243,7 +243,7 @@ namespace WarcraftPlugin.Classes
                 _damageTimer?.Kill();
                 _beamRotationTimer?.Kill();
                 foreach (var beam in _beams)
-                    beam.RemoveIfValid();
+                    WarcraftPlugin.Instance.AddTimer(0.8f, () => beam.RemoveIfValid());
             }
 
             public override void OnTick() { }
