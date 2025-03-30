7f35f5d0d949580c260c30e356f725da3c54115b
ParkwayBE <vandammeben@outlook.com>
2025-03-31 00:09:33 +0200

    added freeze options

--- AdminPanel/AdminPanel.cs
+++ AdminPanel/AdminPanel.cs
@@ -64,6 +64,12 @@ namespace WarcraftPlugin.Core
                 var classMenu = MenuManager.CreateMenu(@$"<font color='lightgrey' class='{FontSizes.FontSizeM}'>
                     {player.SteamID.ToString()}'s Admin Menu</font><br>
                     <font color='grey'>select an option</font> ", 5);
+                //Freeze option
+                classMenu.Add("Freeze bots", null, (pl, opt) =>
+                {
+                    player.ExecuteClientCommandFromServer($"css_freeze @bot");
+                });
+
                 //Kill option
                 classMenu.Add("Kill player menu", null, (pl, opt) =>
                 {
@@ -209,32 +215,6 @@ namespace WarcraftPlugin.Core
                 mess = message;
                 callback(mess);
             };
-        }
-        private async Task<string> RequestInputAsync(CCSPlayerController admin, CCSPlayerController target)
-        {
-            admin.PrintToChat("Write in chat to add argument");
-
-            if (pendingInputs.ContainsKey(admin))
-            {
-                pendingInputs.Remove(admin);
-            }
-
-            string mess = string.Empty;
-
-            // This part sets up the input collection asynchronously
-            pendingInputs[admin] = async (message) =>
-            {
-                mess = message;
-                await Task.CompletedTask; // Ensures the delegate is async
-            };
-
-            // Now, we await for the message to be set by the delegate
-            while (string.IsNullOrEmpty(mess))
-            {
-                await Task.Delay(100); // Add a small delay to prevent a tight loop
-            }
-
-            return mess;
         }
 
         public void ChangeRole(CCSPlayerController player, int role)
