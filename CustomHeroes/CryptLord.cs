cbc665a002102e6187f5e992b48ee6e5f31d1fbc
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-03-30 03:15:40 +0200

    Added new race CryptLord and first abilities

--- CustomHeroes/CryptLord.cs
+++ CustomHeroes/CryptLord.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using CounterStrikeSharp.API.Core;
+using WarcraftPlugin.CustomSkills;
 using WarcraftPlugin.Events.ExtendedEvents;
+using WarcraftPlugin.Helpers;
 using WarcraftPlugin.Models;
+using Vector = CounterStrikeSharp.API.Modules.Utils.Vector;
+
+
 
 namespace WarcraftPlugin.Classes
 {
@@ -43,13 +49,55 @@ namespace WarcraftPlugin.Classes
 
         private void PlayerHurtOther(EventPlayerHurtOther @event)
         {
-            // TODO:  Impale : chance to push enemy up and reducing his gravity temp for the effect
+            if (WarcraftPlayer.GetAbilityLevel(0) > 0)
+            {
+                SkillFunctions.ImpaleTarget(Player, @event.Userid, 600f);
+            }
         }
+
 
         private void PlayerHurt(EventPlayerHurt @event)
         {
-            // TODO: Spiked Carapace : Reflect knife damage skill.
+            if (Player == null || !Player.IsValid) return;
+
+            var attacker = @event.Attacker;
+            if (attacker == null || !attacker.IsValid || attacker == Player) return;
+
+            var victim = @event.Userid;
+            if (victim == null || !victim.IsValid || victim != Player) return;
+
+            var weaponName = attacker.PlayerPawn?.Value?.WeaponServices?.ActiveWeapon?.Value?.DesignerName;
+            if (weaponName != "weapon_knife") return;
+
+            int level = WarcraftPlayer.GetAbilityLevel(1);
+            if (level <= 0) return;
+
+            int damageDealt = @event.DmgHealth;
+
+            float reflectMultiplier = 0.4f * level;
+            int reflectDamage = (int)(damageDealt * reflectMultiplier);
+            var Victimpawn = victim.PlayerPawn.Value;
+
+            int newHealth = Victimpawn.Health + damageDealt;
+            victim.SetHp(newHealth);
+
+            Vector direction = attacker.PlayerPawn.Value.AbsOrigin - Player.PlayerPawn.Value.AbsOrigin;
+            float length = MathF.Sqrt(direction.X * direction.X + direction.Y * direction.Y + direction.Z * direction.Z);
+
+            if (length != 0)
+            {
+                direction = new Vector(direction.X / length, direction.Y / length, direction.Z / length);
+                Vector pushForce = direction * 300.0f; // Adjust force strength here
+                attacker.PlayerPawn.Value.Teleport(null, null, pushForce);
+            }
+
+            SkillFunctions.DealRawDamage(Player, attacker, reflectDamage);
+
+
+            attacker.PrintToCenter($"☠️ You took {reflectDamage} reflected damage from Spiked Carapace!");
+            Player.PrintToCenter($"🛡️ Spiked Carapace reflected {reflectDamage} damage to {attacker.PlayerName}.");
         }
+
 
     }
 }
