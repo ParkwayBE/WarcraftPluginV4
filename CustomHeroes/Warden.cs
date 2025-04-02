fe47ea45dfd82a02fa4b0ae7c9fcf66f0f049580
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-04-02 20:52:48 +0200

    Updated WeaponFire And PlayerShoot event

--- CustomHeroes/Warden.cs
+++ CustomHeroes/Warden.cs
@@ -53,12 +53,23 @@ namespace WarcraftPlugin.Classes
 
         private void PlayerShoot(EventPlayerShoot shoot)
         {
-            Player.PrintToChat("PlayerShoot event has triggered.");
+            var player = shoot.Userid;
+            if (player == null || !player.IsValid)
+                return;
+
+            player.PrintToChat($"{ChatColors.Red}DEBUG:{ChatColors.Default} PlayerShoot event has triggered.");
         }
+
         private void WeaponFire(EventWeaponFire @event)
         {
-            Player.PrintToChat("WeaponFire event has triggered.");
+            var player = @event.Userid;
+            if (player == null || !player.IsValid)
+                return;
+
+            var weaponName = @event.Weapon.ToLower();
+            player.PrintToChat($"{ChatColors.Red}DEBUG:{ChatColors.Default} WeaponFire event has triggered with weapon: {weaponName}");
         }
+
 
 
         private void PlayerDeath(EventPlayerDeath death)
