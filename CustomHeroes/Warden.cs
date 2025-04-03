9e2716ccf5d31ed7c116e069b3fe091b6ed0c8f3
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-04-03 23:53:18 +0200

    even further rollback

--- CustomHeroes/Warden.cs
+++ CustomHeroes/Warden.cs
@@ -162,10 +162,7 @@ namespace WarcraftPlugin.Classes
                 var hitbox = Warcraft.CreateBoxAroundPoint(boxCenter, 20f, 20f, 20f); // width, depth, height
 
                 // Optional: show the hitbox for debugging
-                Console.WriteLine($"[DEBUG] Showing hitbox at: {_grenade.AbsOrigin}");
-                hitbox.Show(Color.Red, 0.05f, 1f);
-
-
+                // hitbox.Show(0.05f, Color.Red, 1);
 
                 foreach (var player in Utilities.GetPlayers())
                 {
