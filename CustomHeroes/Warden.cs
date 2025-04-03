c0884724f0f33a9a032a01d7c9974f73b3430060
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-04-03 17:57:21 +0200

    updated grenade model along hitmarker model

--- CustomHeroes/Warden.cs
+++ CustomHeroes/Warden.cs
@@ -97,6 +97,7 @@ namespace WarcraftPlugin.Classes
             Vector velocity = Player.CalculateVelocityAwayFromPlayer(speed);
             Vector spawnPos = owner.CalculatePositionInFront(60, 75);
 
+            grenade.SetModel("models/props_junk/PopCan01a.vmdl"); // small and nearly invisible
             grenade.Teleport(spawnPos, owner.PlayerPawn.Value.V_angle, new Vector(0, 0, 0));
             grenade.DispatchSpawn();
 
