27f5908b63d0dcbd0571952b447d122af1a96c39
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-04-10 19:37:58 +0200

    fixed if statement

--- CustomHeroes/DwarvenSniper.cs
+++ CustomHeroes/DwarvenSniper.cs
@@ -246,8 +246,7 @@ namespace WarcraftPlugin.Classes
             var attacker = @event.Attacker;
             var victim = @event.Userid;
 
-            if (attacker.TeamNum == victim.TeamNum)
-                return;
+            if (attacker.TeamNum == victim.TeamNum) return;
 
             if (@event.Weapon == "ssg08" || @event.Weapon == "awp")
             {
