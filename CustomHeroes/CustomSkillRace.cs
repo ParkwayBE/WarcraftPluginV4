3e1b16f503528f5194c5a31d95cdf1d8ac5ce0b6
IIPoisoned-wcs <matthias.janssens1996@hotmail.com>
2025-04-22 05:19:07 +0200

    Re-added CustomSkillRace

---
+++ CustomHeroes/CustomSkillRace.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using CounterStrikeSharp.API.Core;
+using WarcraftPlugin.CustomSkills;
+using WarcraftPlugin.Models;
+
+
+
+namespace WarcraftPlugin.Classes
+{
+    public class CustomSkillRace : WarcraftClass
+    {
+        public override string DisplayName => "CustomSkillRace";
+
+        public override Color DefaultColor => Color.GreenYellow;
+
+        public override List<IWarcraftAbility> Abilities =>
+        [
+            new WarcraftAbility("TEST MOVEMENT SPEED", "STEST"),
+            new WarcraftAbility("TEST HEALTH", "TEST"),
+            new WarcraftAbility("TEST INVISIBILITY", "TEST"),
+            new WarcraftCooldownAbility("TEST TELEPORT", " TEST ", 5f)
+        ];
+
+        public override void Register()
+        {
+            HookEvent<EventPlayerPing>(OnPlayerPing);
+            HookAbility(3, Ultimate);
+        }
+
+        private void Ultimate()
+        {
+            SkillFunctions.TeleportUltimate(Player);
+            StartCooldown(3); // Index 3 = Ultimate
+        }
+
+        private void OnPlayerPing(EventPlayerPing ping)
+        {
+            SkillFunctions.HandleTeleportPing(Player, ping.X, ping.Y, ping.Z);
+
+
+
+
+            // Storage working particles
+            // particles/weapons/cs_weapon_fx/weapon_snowball_impact_splash.vpcf
+            //
+        }
+    }
+}
